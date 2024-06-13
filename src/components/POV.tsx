0c84d7628abb46df4745ad79d4e76d2dae4af1fe
Juan Madalena <juanmadalena06@gmail.com>
2024-06-13 05:29:30 +0200

    test IOS

--- src/components/POV.tsx
+++ src/components/POV.tsx
@@ -8,17 +8,15 @@ import useOrientationPermission from "../hooks/useOrientationPermission";
 function POV() {
     const { camera, gl } = useThree();
     const { isMobile } = useDetectDevice();
-    const { paintings, init, } = useContext(LoaderContext)
+    const { init, } = useContext(LoaderContext)
     const { permission } = useOrientationPermission()
 
     return (
         <>
             {
-                paintings?.length > 0 || init ? null : isMobile ?
-                    permission === 'granted' ?
+                init ? null : isMobile && permission === 'granted' ?
                     <DeviceOrientationControls camera={camera} deviceOrientation={{ alpha: window.orientation, beta: 100, gamma: 0 }} />
-                    : null
-                :
+                    :
                     <PointerLockControls camera={camera} domElement={gl.domElement} maxPolarAngle={Math.PI / 2} />
             }
         </>
